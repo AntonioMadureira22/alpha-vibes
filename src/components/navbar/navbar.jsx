7ba1668d1b05169cd408d247feb7f4d9b059b2a4
AntonioMadureira22 <you@example.com>
2022-11-15 22:51:51 -0500

    initial

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../navbar/navbar.css'
+const navbar = () => {
+  return (
+    <div className='top'>
+        <div className="wrapper">
+            <div className="left">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default navbar
